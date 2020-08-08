653202d793cdcd82af7414a4572d16f963826d66
MaratArkhipov <maratkzn2010@yandex.ru>
2020-08-08 20:14:59 +0400

    added unit-test: readJsonSuccessTest added Hamcrest-test

--- src/test/java/readJsonSuccessTest.java
+++ src/test/java/readJsonSuccessTest.java
@@ -3,12 +3,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class readJsonSuccessTest {
     @Test
     void readJson() throws FileNotFoundException {
         FileReader originalReaderJson = new FileReader("data.json");
         Gson gson = new Gson();
-        Assertions.assertNotNull(gson.fromJson(originalReaderJson, Employee[].class));
+
+//        Assertions.assertNotNull(gson.fromJson(originalReaderJson, Employee[].class));
+
+        assertThat(gson.fromJson(originalReaderJson, Employee[].class),notNullValue());
     }
 }
